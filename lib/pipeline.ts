40748f0d80ec957a95267fae06a59c46fbe91dcf
Dan Marchese <dan@marchese.me>
2021-11-06 15:10:21 -0700

    Switching to GitHub secret and renaming the pipeline

--- lib/pipeline.ts
+++ lib/pipeline.ts
@@ -1,15 +1,18 @@
 import * as cdk from '@aws-cdk/core';
 import { CodePipeline, CodePipelineSource, ShellStep } from '@aws-cdk/pipelines';
 import { AirConditioningStage } from './app';
+import { SecretValue } from '@aws-cdk/core';
 
 export class AirConditioningCdkStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const pipeline = new CodePipeline(this, 'CodePipeline', {
-      pipelineName: 'ACRegistryPipeline',
+      pipelineName: 'AirConditioningPipeline',
       synth: new ShellStep('Synth', {
-        input: CodePipelineSource.gitHub('marchese29/AirConditioningCDK', 'main'),
+        input: CodePipelineSource.gitHub('marchese29/AirConditioningCDK', 'main', {
+          authentication: SecretValue.secretsManager('arn:aws:secretsmanager:us-west-2:516158321459:secret:marchese29GitHubToken-Y9DYru')
+        }),
         commands: ['npm ci', 'npm run build', 'npx cdk synth']
       })
     });
